aedacaa0274f2f0cf815f6fbfacfd7e9d6979b96
Documenter.jl <documenter@juliadocs.github.io>
2025-09-29 18:17:47 +0000

    build based on 452ee92

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"#MyPkg","page":"Home","title":"MyPkg","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MyPkg.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+[{"location":"#MyPkg","page":"Home","title":"MyPkg","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MyPkg.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"#MyPkg.foo-Tuple{Number, Number}","page":"Home","title":"MyPkg.foo","text":"foo(x, y)\n\nCreates a 2-element static array from the scalars x and y.\n\n\n\n\n\n","category":"method"}]
 }
