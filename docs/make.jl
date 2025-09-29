d39b09548c28ab7b9ffc1aef315fc332da23070b
fcabral3 <fcabral1290@gmail.com>
2025-09-29 17:00:58 +0000

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.56

---
+++ docs/make.jl
@@ -0,0 +1,23 @@
+using MyPkg
+using Documenter
+
+DocMeta.setdocmeta!(MyPkg, :DocTestSetup, :(using MyPkg); recursive=true)
+
+makedocs(;
+    modules=[MyPkg],
+    authors="Filipe Cabral",
+    sitename="MyPkg.jl",
+    format=Documenter.HTML(;
+        canonical="https://fcabral3.github.io/MyPkg.jl",
+        edit_link="main",
+        assets=String[],
+    ),
+    pages=[
+        "Home" => "index.md",
+    ],
+)
+
+deploydocs(;
+    repo="github.com/fcabral3/MyPkg.jl",
+    devbranch="main",
+)
